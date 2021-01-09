18c93159e77f85ec498a8147ba876bf0e059f79e
Peter Gagarinov <pgagarinov@gmail.com>
2021-01-09 20:02:15 +0300

    Added segmentation model comparison example

--- products/jupyterlab-ml-devenv/conda_env_yaml_strip_versions_example.sh
+++ products/jupyterlab-ml-devenv/conda_env_yaml_strip_versions_example.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mlenvtool conda_env_yaml_transform versions_eq2ge ./mldevenv_conda_requirements.yml ./out.yml --except_package_list 'pytorch' 'torchvision'
+mlenvtool conda_env_yaml_transform versions_eq2ge ./mldevenv_conda_requirements.yml ./mldevenv_conda_requirements_no_ver.yml --except_package_list 'pytorch' 'torchvision'
